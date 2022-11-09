46d1ff65681a2192c439b523ecf9b06f06955329
SadeenC <bsadeen9@gmail.com>
2022-11-09 16:07:45 +0530

    Update route/authRoute.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
 const authRouter = require('./route/authRoute')
+const cookieParser = require('cookie-parser')
 
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use("/auth",authRouter)
 
--- route/authRoute.js
+++ route/authRoute.js
@@ -1,12 +1,16 @@
-
+require('dotenv').config()
 const {Router} = require('express')
 const userModel = require('../models/user')
+const jwt  = require('jsonwebtoken')
+
 
 const authRouter = Router()
 
 authRouter.post('/signup',async(req,res)=>{
     try {
         const user = await userModel.create(req.body)
+        const tkn = jwt.sign({user},process.env.JWT_SECRET,{expiresIn:'1h'})
+        res.cookie("jwt",tkn,{maxAge:60*60*60})
         res.status(200).json(user)
     } catch (error) {
         res.status(400).json({error:error.message})
