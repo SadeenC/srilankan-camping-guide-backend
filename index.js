9d6a8a4b7b187bac80a44b7fea9261476afcea7f
SadeenC <bsadeen9@gmail.com>
2022-11-09 14:51:08 +0530

    initilazing

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose')
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+const dbURL = process.env.DATABASE_URL
+
+mongoose.connect(dbURL,{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    app.listen(3000,()=>{
+        console.log("database connected!");
+        console.log('server runing....');
+    })
+})
+.catch(err=>{
+    console.log({error:err.message});
+})
+
+
+
+
